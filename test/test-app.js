3f0df443f910a349bf27b9dc375709b85c3ffe4e
Anand M.S <anand.m.s@sap.com>
2014-11-26 15:31:40 +0530

    Updated test
    - checks all file generation

--- test/test-app.js
+++ test/test-app.js
@@ -12,15 +12,58 @@ describe('jekyllstarter:app', function () {
       .inDir(path.join(os.tmpdir(), './temp-test'))
       .withOptions({ 'skip-install': true })
       .withPrompt({
-        someOption: true
+        someOption: true,
+        blogTitle:'Hello Blog!',
+        tagline:'This is a test blog',
+        desc:'This is the description',
+        blogUrl:'http://www.test.com',
+        authorName:'Anand Mani Sankar',
+        twitterHandle:'msanand',
+        disqusId:'disqusid',
+        addthisPubId:'addthispubid',
+        gaTrackingId:'gatrackingid',
+        gaSuperProxyUrl:'http://www.gasuperproxyurl.com',
+        githubRepo:'http://github.com/msanand',
+        feedbackUrl:'http://github.com/msanand/feedback/issues/new'
       })
       .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
+      '404.html',
+      'about.md',
+      'archive.md',
+      'atom.xml',
+      'Gruntfile.js',
+      'index.html',
+      'LICENSE',
+      'LICENSE.md',
+      'tags.html',
+      '_drafts/draft-post.md',
+      '_includes/add_this.html',
+      '_includes/comments.html',
+      '_includes/follow_me.html',
+      '_includes/ga_data_fetch.html',
+      '_includes/google_analytics.html',
+      '_includes/head.html',
+      '_includes/sidebar.html',
+      '_layouts/default.html',
+      '_layouts/page.html',
+      '_layouts/post.html',
+      '_posts/2014-08-15-hello-jekyll-starter.md',
+      'assets/css/all.min.css',
+      'assets/css/custom.css',
+      'assets/css/lanyon.css',
+      'assets/css/poole.css',
+      'assets/css/syntax.css',
+      'assets/images/profile.png',
+      'assets/images/profile_small.png',
+      'assets/favicon.ico',
+      'assets/apple-touch-icon-precomposed.png',
       'bower.json',
       'package.json',
+      'README.md',
       '.editorconfig',
       '.jshintrc'
     ]);
